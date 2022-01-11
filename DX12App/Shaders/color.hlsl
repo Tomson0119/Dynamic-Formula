1f752663291e0d939d20f1b767140b867b24a962
Tomson0119 <kjs980119@gmail.com>
2022-01-11 11:20:41 +0900

    엠비언트 항 적용, 텍스쳐 변환 함수 적용

--- DX12App/Shaders/color.hlsl
+++ DX12App/Shaders/color.hlsl
@@ -33,17 +33,16 @@ VertexOut VS(VertexIn vin)
     vout.NormalW = mul((float3x3) tWorld, vin.NormalL);
     vout.TangentW = mul((float3x3) tWorld, vin.TangentL);
     
-    vout.TexCoord = vin.TexCoord;
+    float4 texC = mul(float4(vin.TexCoord, 0.0f, 1.0f), gTexTransform);
+    vout.TexCoord = texC.xy;
 	
     return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-    float4 diffuse = gDiffuseMap.Sample(gAnisotropicWrap, pin.TexCoord) * gMat.Diffuse;
-    
     float3 view = normalize(gCameraPos - pin.PosW);
-    float4 ambient = gAmbient * diffuse;
+    float4 ambient = gAmbient * float4(gMat.Ambient, 1.0f) * gMat.Diffuse;
     
     float shadowFactor[3] = { 1.0f, 1.0f, 1.0f };
     for (int i = 0; i < 3; i++)
@@ -51,10 +50,9 @@ float4 PS(VertexOut pin) : SV_Target
         shadowFactor[i] = CalcShadowFactor(pin.PosS);
     }
     
-    Material mat = { diffuse, gMat.Fresnel, gMat.Roughness };
-    float4 directLight = ComputeLighting(gLights, mat, pin.NormalW, view, shadowFactor);
+    float4 directLight = ComputeLighting(gLights, gMat, pin.NormalW, view, shadowFactor);
     
     float4 result = ambient + directLight;
-    result.a = diffuse.a;
+    result.a = gMat.Diffuse.a;
     return result;
 }
