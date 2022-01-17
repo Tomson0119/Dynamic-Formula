7aeb1369188fae2b3f43f9794b2f70c97a6c4a74
Tomson0119 <kjs980119@gmail.com>
2022-01-17 19:04:32 +0900

    feat - 변수와 패킷, enum 클래스 추가
    이름과 패스워드, 방의 최대 개수와 수용 인원을 정의한다.
    로그인 시도 및 방 입장 요청 패킷을 추가하고 그에 따른 결과를 나타내는 패킷도 추가함.
    로그인 실패의 이유를 나타내는 LOGIN_STAT과 방 입장에 실패했을 때 방의 상태를 나타내는 ROOM_STAT을 추가함.

--- Server/Network/Protocol.h
+++ Server/Network/Protocol.h
@@ -2,71 +2,85 @@
 
 const short SERVER_PORT = 4000;
 
-const int  MAX_NAME_SIZE = 20;
-const int  MAX_CHAT_SIZE = 100;
-const int  MAX_PLAYER = 10000;
+const int MAX_NAME_SIZE  = 20;
+const int MAX_PWD_SIZE = 40;
+
+const int MAX_PLAYER_SIZE = 10000;
+const int MAX_ROOM_CAPACITY = 8;
+const int MAX_ROOM_SIZE = MAX_PLAYER_SIZE / MAX_ROOM_CAPACITY + 1;
 
 const int MaxBufferSize = 300;
+
+enum class LOGIN_STAT : char
+{
+	ACCEPTED = 0,
+	INVALID_ID,
+	ALREADY_IN,
+	BANNED
+};
+
+enum class ROOM_STAT : char
+{
+	GAME_STARTED = 0,
+	ROOM_IS_FULL,
+	MAX_ROOM_REACHED
+};
 
 #pragma pack (push, 1)
 namespace CS
 {
 	const char LOGIN = 1;
-	const char MOVE = 2;
-	const char CHAT = 3;
+	const char ENTER_ROOM = 2;
+	const char MOVE = 3;
+	const char CHAT = 4;
 
-	struct login_packet {
+	struct packet_login {
 		unsigned char size;
-		char	type;
-		char	name[MAX_NAME_SIZE];
+		char type;
+		char name[MAX_NAME_SIZE];
+		char pwd[MAX_PWD_SIZE];
 	};
 
-	struct move_packet {
+	struct packet_enter_room {
 		unsigned char size;
-		char	type;
-		char	direction;
-		int		move_time;
-	};
-
-	struct chat_packet {
-		unsigned char size;
-		char	type;
-		char	message[MAX_CHAT_SIZE];
+		char type;
+		bool new_room;
+		int room_id;
 	};
 }
 
 namespace SC
 {
-	const char LOGIN_OK = 1;
-	const char MOVE = 2;
-	const char CHAT = 3;
-	const char LOGIN_FAIL = 4;
-
-	struct packet_login_ok {
-		unsigned char size;
-		char type;
-		int	 id;
+	struct PlayerState
+	{
+		char name[MAX_NAME_SIZE];
+		bool empty;
+		bool ready;
+		char color;
 	};
 
-	struct packet_login_fail {
+	const char LOGIN_RESULT = 1;
+	const char ENTER_ROOM_ACCEPT = 2;
+	const char ENTER_ROOM_DENY = 3;
+	const char MOVE = 4;
+	const char CHAT = 5;
+
+	struct packet_login_result {
+		unsigned char size;
+		char type;
+		char result;
+	};
+
+	struct packet_enter_room_accept {
+		unsigned char size;
+		char type;
+		PlayerState player_stats[MAX_ROOM_CAPACITY];
+	};
+
+	struct packet_enter_room_deny {
 		unsigned char size;
 		char type;
 		char reason;
-	};
-
-	struct packet_move {
-		unsigned char size;
-		char  type;
-		int	  id;
-		short x, y;
-		int	  move_time;
-	};
-
-	struct packet_chat {
-		unsigned char size;
-		char type;
-		int  id;
-		char message[MAX_CHAT_SIZE];
 	};
 }
 #pragma pack(pop)
