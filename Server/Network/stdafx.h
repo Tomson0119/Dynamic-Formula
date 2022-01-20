9a62b2f3f40e2616d3301d4388ed6ac77effc6e9
Tomson0119 <kjs980119@gmail.com>
2022-01-20 19:48:27 +0900

    feat - 패킷의 타입을 반환하는 함수, const char* 문자열을 wstring으로 변환하는 함수를 inline으로 정의함.

--- Server/Network/stdafx.h
+++ Server/Network/stdafx.h
@@ -26,6 +26,7 @@ extern "C" {
 #include <unordered_map>
 #include <unordered_set>
 #include <concurrent_priority_queue.h>
+#include <concurrent_unordered_set.h>
 
 #include "NetException.h"
 #include "Protocol.h"
@@ -33,3 +34,17 @@ extern "C" {
 #define ABS(x) ((x < 0) ? -(x) : (x))
 
 using namespace std::chrono_literals;
+
+inline char GetPacketType(std::byte* pck)
+{
+	return *reinterpret_cast<char*>(pck + sizeof(uint16_t));
+}
+
+inline std::wstring CharToWString(const char* str)
+{
+	std::string s_str(str);
+	int size = MultiByteToWideChar(CP_ACP, 0, &s_str[0], (int)s_str.size(), NULL, 0);
+	std::wstring wstr(size, 0);
+	MultiByteToWideChar(CP_ACP, 0, &s_str[0], (int)s_str.size(), &wstr[0], size);
+	return wstr;
+}
