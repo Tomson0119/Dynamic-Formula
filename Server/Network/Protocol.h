c613b8e6310ca23501422d20a774ca8fbbc1b4d0
Tomson0119 <kjs980119@gmail.com>
2022-01-20 19:46:48 +0900

    feat - 프로토콜 수정
    pwd size를 20으로 수정, 사용자 등록의 결과를 나타내는 REGI_STAT 클래스 추가
    사용자 등록 패킷 구조체 추가, 패킷의 크기를 unsigned char 에서 unsigned short로 변경

--- Server/Network/Protocol.h
+++ Server/Network/Protocol.h
@@ -3,7 +3,7 @@
 const short SERVER_PORT = 4000;
 
 const int MAX_NAME_SIZE  = 20;
-const int MAX_PWD_SIZE = 40;
+const int MAX_PWD_SIZE = 20;
 
 const int MAX_PLAYER_SIZE = 10000;
 const int MAX_ROOM_CAPACITY = 8;
@@ -12,11 +12,16 @@ const int MAX_ROOM_SIZE = MAX_PLAYER_SIZE / MAX_ROOM_CAPACITY + 1;
 const int MaxBufferSize = 300;
 
 enum class LOGIN_STAT : char
+{	
+	INVALID_IDPWD = -2,
+	ACCEPTED,
+};
+
+enum class REGI_STAT : char
 {
 	ACCEPTED = 0,
 	INVALID_IDPWD,
-	ALREADY_IN,
-	BANNED
+	ALREADY_EXIST
 };
 
 enum class ROOM_STAT : char
@@ -30,25 +35,31 @@ enum class ROOM_STAT : char
 namespace CS
 {
 	const char LOGIN = 1;
-	const char OPEN_ROOM = 2;
-	const char ENTER_ROOM = 3;
-	const char MOVE = 3;
-	const char CHAT = 4;
+	const char REGISTER = 2;
+	const char OPEN_ROOM = 3;
+	const char ENTER_ROOM = 4;
 
 	struct packet_login {
-		unsigned char size;
+		uint16_t size;
+		char type;
+		char name[MAX_NAME_SIZE];
+		char pwd[MAX_PWD_SIZE];
+	};
+
+	struct packet_register {
+		uint16_t size;
 		char type;
 		char name[MAX_NAME_SIZE];
 		char pwd[MAX_PWD_SIZE];
 	};
 
 	struct packet_open_room {
-		unsigned char size;
+		uint16_t size;
 		char type;
 	};
 
 	struct packet_enter_room {
-		unsigned char size;
+		uint16_t size;
 		char type;
 		int room_id;
 	};
@@ -65,25 +76,32 @@ namespace SC
 	};
 
 	const char LOGIN_RESULT = 1;
-	const char ACCESS_ROOM_ACCEPT = 2;
-	const char ACCESS_ROOM_DENY = 3;
-	const char MOVE = 4;
-	const char CHAT = 5;
+	const char REGISTER_RESULT = 2;
+	const char ACCESS_ROOM_ACCEPT = 3;
+	const char ACCESS_ROOM_DENY = 4;
 
 	struct packet_login_result {
-		unsigned char size;
+		uint16_t size;
+		char type;
+		char result;
+	};
+
+	struct packet_register_result {
+		uint16_t size;
 		char type;
 		char result;
 	};
 
 	struct packet_access_room_accept {
-		unsigned char size;
+		uint16_t size;
 		char type;
+		char map_id;
+		int room_id;
 		PlayerState player_stats[MAX_ROOM_CAPACITY];
 	};
 
 	struct packet_access_room_deny {
-		unsigned char size;
+		uint16_t size;
 		char type;
 		char reason;
 	};
