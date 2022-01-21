f51864809b96c007e67b399abe395bdbefcb53c4
Tomson0119 <kjs980119@gmail.com>
2022-01-21 18:12:16 +0900

    feat - 패킷 구조체 추가 및 이름 변경
    클라이언트에서 Revert Scene 패킷을 추가함.
    방 업데이트 패킷에서 해당 방이 닫혔는지 여부를 같이 전송함.

--- Server/Network/Protocol.h
+++ Server/Network/Protocol.h
@@ -19,16 +19,18 @@ enum class LOGIN_STAT : char
 
 enum class REGI_STAT : char
 {
-	ACCEPTED = 0,
-	INVALID_IDPWD,
-	ALREADY_EXIST
+	INVALID_IDPWD = 0,
+	ALREADY_EXIST,
+	ACCEPTED
 };
 
 enum class ROOM_STAT : char
 {
 	GAME_STARTED = 0,
 	ROOM_IS_FULL,
-	MAX_ROOM_REACHED
+	ROOM_IS_CLOSED,
+	MAX_ROOM_REACHED,
+	AVAILABLE
 };
 
 #pragma pack (push, 1)
@@ -38,30 +40,41 @@ namespace CS
 	const char REGISTER = 2;
 	const char OPEN_ROOM = 3;
 	const char ENTER_ROOM = 4;
+	const char REVERT_SCENE = 5;
 
-	struct packet_login {
+	struct packet_login 
+	{
 		uint16_t size;
 		char type;
 		char name[MAX_NAME_SIZE];
 		char pwd[MAX_PWD_SIZE];
 	};
 
-	struct packet_register {
+	struct packet_register 
+	{
 		uint16_t size;
 		char type;
 		char name[MAX_NAME_SIZE];
 		char pwd[MAX_PWD_SIZE];
 	};
 
-	struct packet_open_room {
+	struct packet_open_room 
+	{
 		uint16_t size;
 		char type;
 	};
 
-	struct packet_enter_room {
+	struct packet_enter_room
+	{
 		uint16_t size;
 		char type;
 		int room_id;
+	};
+
+	struct packet_revert_scene
+	{
+		uint16_t size;
+		char type;
 	};
 }
 
@@ -77,23 +90,26 @@ namespace SC
 
 	const char LOGIN_RESULT = 1;
 	const char REGISTER_RESULT = 2;
-	const char ACCESS_ROOM_ACCEPT = 3;
+	const char WAIT_PLAYERS_INFO = 3;
 	const char ACCESS_ROOM_DENY = 4;
-	const char ROOM_INFO = 5;
+	const char ROOM_UPDATE_INFO = 5;
 
-	struct packet_login_result {
+	struct packet_login_result 
+	{
 		uint16_t size;
 		char type;
 		char result;
 	};
 
-	struct packet_register_result {
+	struct packet_register_result 
+	{
 		uint16_t size;
 		char type;
 		char result;
 	};
 
-	struct packet_access_room_accept {
+	struct packet_wait_players_info 
+	{
 		uint16_t size;
 		char type;
 		char map_id;
@@ -101,19 +117,22 @@ namespace SC
 		PlayerState player_stats[MAX_ROOM_CAPACITY];
 	};
 
-	struct packet_access_room_deny {
+	struct packet_access_room_deny 
+	{
 		uint16_t size;
 		char type;
 		char reason;
 	};
 
-	struct packet_room_info {
+	struct packet_room_update_info 
+	{
 		uint16_t size;
 		char type;
 		int room_id;
 		unsigned char player_count : 4;
 		unsigned char map_id : 1;
 		bool game_started : 1;
+		bool room_closed : 1;
 	};
 }
 #pragma pack(pop)
