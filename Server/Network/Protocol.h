c2a338d8b09515799a533d8a897871479d793a6c
Tomson0119 <kjs980119@gmail.com>
2022-01-24 20:45:58 +0900

    feat - 패킷 헤더를 분리하여 각 패킷이 이를 상속, 패킷 추가
    특정 플레이어 업데이트 패킷, 맵 변경 패킷, 플레이어 제거 패킷을 추가함.

--- Server/Network/Protocol.h
+++ Server/Network/Protocol.h
@@ -35,6 +35,12 @@ enum class ROOM_STAT : char
 };
 
 #pragma pack (push, 1)
+struct packet_header 
+{
+	uint16_t size;
+	char type;
+};
+
 namespace CS
 {
 	const char LOGIN		= 1;
@@ -42,40 +48,39 @@ namespace CS
 	const char OPEN_ROOM	= 3;
 	const char ENTER_ROOM	= 4;
 	const char REVERT_SCENE = 5;
+	const char SWITCH_MAP   = 6;
+	const char PRESS_READY  = 7;
 
-	struct packet_login 
+	struct packet_login : packet_header
 	{
-		uint16_t size;
-		char type;
 		char name[MAX_NAME_SIZE];
 		char pwd[MAX_PWD_SIZE];
 	};
 
-	struct packet_register 
+	struct packet_register : packet_header
 	{
-		uint16_t size;
-		char type;
 		char name[MAX_NAME_SIZE];
 		char pwd[MAX_PWD_SIZE];
 	};
 
-	struct packet_open_room 
-	{
-		uint16_t size;
-		char type;
-	};
+	struct packet_open_room : packet_header { };
 
-	struct packet_enter_room
+	struct packet_enter_room : packet_header
 	{
-		uint16_t size;
-		char type;
 		int room_id;
 	};
 
-	struct packet_revert_scene
+	struct packet_revert_scene : packet_header { };
+
+	struct packet_switch_map : packet_header
 	{
-		uint16_t size;
-		char type;
+		char map_id;
+		char room_id;
+	};
+
+	struct packet_press_ready : packet_header
+	{
+		char room_id;
 	};
 }
 
@@ -96,59 +101,65 @@ namespace SC
 	const char ACCESS_ROOM_DENY   = 4;
 	const char ROOM_INSIDE_INFO   = 5;
 	const char ROOM_OUTSIDE_INFO  = 6;
+	const char UPDATE_PLAYER_INFO = 7;
+	const char UPDATE_MAP_INFO    = 8;
+	const char REMOVE_PLAYER	  = 9;
 
-	struct packet_force_logout
-	{
-		uint16_t size;
-		char type;
-	};
+	struct packet_force_logout : packet_header { };
 
-	struct packet_login_result 
+	struct packet_login_result : packet_header 
 	{
-		uint16_t size;
-		char type;
 		char result;
 	};
 
-	struct packet_register_result 
+	struct packet_register_result : packet_header
 	{
-		uint16_t size;
-		char type;
 		char result;
 	};
 
-	struct packet_access_room_accept
+	struct packet_access_room_accept : packet_header
 	{
-		uint16_t size;
-		char type;
 		int room_id;
 	};
 
-	struct packet_access_room_deny
+	struct packet_access_room_deny : packet_header
 	{
-		uint16_t size;
-		char type;
 		char reason;
 	};
 
-	struct packet_room_inside_info 
+	struct packet_room_inside_info : packet_header
 	{
-		uint16_t size;
-		char type;
 		char map_id;
 		int room_id;		
 		PlayerState player_stats[MAX_ROOM_CAPACITY];
 	};
 
-	struct packet_room_outside_info 
+	struct packet_room_outside_info : packet_header
 	{
-		uint16_t size;
-		char type;
 		int room_id;
 		unsigned char player_count : 4;
 		unsigned char map_id : 1;
 		bool game_started : 1;
 		bool room_closed : 1;
+	};
+
+	struct packet_update_player_info : packet_header
+	{
+		int room_id;
+		char index;
+		PlayerState player_info;
+	};
+
+	struct packet_update_map_info : packet_header
+	{
+		int room_id;
+		char map_id;
+	};
+
+	struct packet_remove_player : packet_header
+	{
+		int room_id;
+		char index;
 	};
 }
 #pragma pack(pop)
