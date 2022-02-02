862a0e862ba906053ff936351b3be41f75bb0313
Tomson0119 <kjs980119@gmail.com>
2022-02-02 17:07:22 +0900

    misc - 물리엔진 라이브러리 추가

--- Server/common.h
+++ Server/common.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define BT_NO_SIMD_OPERATOR_OVERLOADS
+#define NOMINMAX
+
 #include <stdafx.h>
 #include <Socket.h>
 #include <EndPoint.h>
@@ -8,6 +11,11 @@
 #include <Protocol.h>
 #include <IOCP.h>
 #include <MemoryPool.h>
-#include <Timer.h>
 
-//#define DEBUG_PACKET_TRANSFER
+#include "Timer.h"
+
+#include "btBulletDynamicsCommon.h"
+#include "BulletCollision/CollisionShapes/btHeightfieldTerrainShape.h"
+
+
+#define DEBUG_PACKET_TRANSFER
