30d603e5859868406b229424813bc9b69b3a55e7
Tomson0119 <kjs980119@gmail.com>
2022-02-03 20:15:59 +0900

    feat - BulletHelper 네임스페이스, 함수 추가

--- Server/common.h
+++ Server/common.h
@@ -19,3 +19,32 @@
 
 
 #define DEBUG_PACKET_TRANSFER
+
+namespace BulletHelper
+{
+	const float PI = 3.1415926535f;
+	inline btRigidBody* CreateRigidBody(btScalar mass, const btTransform& startTransform, btCollisionShape* shape, std::shared_ptr<btDiscreteDynamicsWorld> pbtDynamicsWorld)
+	{
+		btAssert((!shape || shape->getShapeType() != INVALID_SHAPE_PROXYTYPE));
+
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic && shape)
+			shape->calculateLocalInertia(mass, localInertia);
+
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+
+		btRigidBody::btRigidBodyConstructionInfo cInfo(mass, myMotionState, shape, localInertia);
+
+		btRigidBody* body = new btRigidBody(cInfo);
+
+		pbtDynamicsWorld->addRigidBody(body);
+		return body;
+	}
+
+	inline float RadianToEuler(float fRadian)
+	{
+		return fRadian / 360 * (float)(2 * PI);
+	}
+}
