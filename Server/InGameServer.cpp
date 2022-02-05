87ef6efd91c9145b36a3957a96cb2fc4ebe5ae5a
Tomson0119 <kjs980119@gmail.com>
2022-02-05 20:47:03 +0900

    feat - 인게임서버 기능 추가
    인 게임 서버는 게임이 시작되면 로비 서버로부터 플레이어 아이디 배열을 전달받아
    해당하는 월드에 대해서 플레이어를 초기화한다.
    클라이언트로부터 키 입력을 받아서 키 입력에 따라 행동을 처리할 수 있도록 함.

--- Server/InGameServer.cpp
+++ Server/InGameServer.cpp
@@ -1,6 +1,77 @@
 #include "common.h"
+#include "LoginServer.h"
+#include "LobbyServer.h"
 #include "InGameServer.h"
+#include "Client.h"
+
+InGameServer::WorldList InGameServer::msWorlds;
 
 InGameServer::InGameServer()
+{	
+}
+
+void InGameServer::Init(LoginServer* loginPtr)
 {
+	mLoginPtr = loginPtr;
+}
+
+void InGameServer::PrepareToStartGame(int roomID, const std::array<int, MAX_ROOM_CAPACITY>& ids)
+{
+	// TODO: Initialize players..
+}
+
+bool InGameServer::ProcessPacket(std::byte* packet, char type, int id, int bytes)
+{
+	switch (type)
+	{
+	case CS::KEY_INPUT:
+	{
+#ifdef DEBUG_PACKET_TRANSFER
+		std::cout << "[" << id << "] Received key input packet.\n";
+#endif
+		CS::packet_key_input* pck = reinterpret_cast<CS::packet_key_input*>(packet);
+		if (pck->room_id < 0)
+		{
+			mLoginPtr->Disconnect(id);
+			break;
+		}
+		HandleKeyInput(id, pck->key, pck->pressed);
+		break;
+	}
+	default:
+		std::cout << "Invalid packet.\n";
+		return false;
+	}
+	return true;
+}
+
+void InGameServer::HandleKeyInput(int id, uint8_t key, bool pressed)
+{
+	switch (static_cast<int>(key))
+	{
+	case VK_LEFT:
+		break;
+
+	case VK_RIGHT:
+		break;
+
+	case VK_UP:
+		break;
+
+	case VK_DOWN:
+		break;
+
+	case VK_LSHIFT:
+		break;
+
+	case 'Z':
+		break;
+
+	case 'X':
+		break;
+
+	default:
+		std::cout << "Invalid key input.\n";
+		break;
+	}
 }
--- Server/InGameServer.h
+++ Server/InGameServer.h
@@ -1,9 +1,32 @@
 #pragma once
 
+#include "BPHandler.h"
+#include "Player.h"
+
+class LoginServer;
+
+struct GameWorld
+{
+	BPHandler Physics;
+	std::array<Player, MAX_ROOM_CAPACITY> PlayerList;
+};
+
 class InGameServer
 {
+	using WorldList = std::array<std::unique_ptr<GameWorld>, MAX_ROOM_SIZE>;
 public:
 	InGameServer();
 	~InGameServer() = default;
 
+	void Init(LoginServer* loginPtr);
+	void PrepareToStartGame(int roomID, const std::array<int, MAX_ROOM_CAPACITY>& ids);
+
+	bool ProcessPacket(std::byte* packet, char type, int id, int bytes);
+	void HandleKeyInput(int id, uint8_t key, bool pressed);
+
+
+private:
+	LoginServer* mLoginPtr;
+
+	static WorldList msWorlds;
 };
