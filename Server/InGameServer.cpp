904c19bf2c00bf1481aad164e53a1accb8f7d063
Tomson0119 <kjs980119@gmail.com>
2022-02-07 18:05:58 +0900

    feat - 인게임 씬 초기화 및 게임 시작 처리
    인게임은 처음 초기화할 때, 모든 방을 레퍼런스로 받아서 해당 방의 플레이어정보를 포인터로 저장한다.
    게임이 시작되면 모든 클라이언트의 상태를 in_game으로 변경하고 모두에게 게임 시작 패킷을 초기위치정보와 함께 보낸다.

--- Server/InGameServer.cpp
+++ Server/InGameServer.cpp
@@ -1,23 +1,54 @@
 #include "common.h"
-#include "LoginServer.h"
-#include "LobbyServer.h"
 #include "InGameServer.h"
+#include "LoginServer.h"
+#include "WaitRoom.h"
 #include "Client.h"
+#include "Player.h"
 
 InGameServer::WorldList InGameServer::msWorlds;
 
 InGameServer::InGameServer()
+	: mLoginPtr{ nullptr }
 {	
 }
 
-void InGameServer::Init(LoginServer* loginPtr)
+void InGameServer::Init(LoginServer* loginPtr, RoomList& roomList)
 {
 	mLoginPtr = loginPtr;
+
+	const float offset_x = 20.0f;
+	for (int i = 0; i < MAX_ROOM_SIZE; i++)
+	{
+		msWorlds[i] = std::make_unique<GameWorld>();
+		msWorlds[i]->Physics.Init(-10.0f);
+		msWorlds[i]->MapIdx = roomList[i]->GetMapIndex();
+
+		for (int j = 0; auto& player : msWorlds[i]->PlayerList)
+		{
+			player = roomList[i]->GetPlayerPtr(j);
+			player->SetPosition(
+				mStartPosition.x() + offset_x * j,
+				mStartPosition.y(),
+				mStartPosition.z());
+			j++;
+		}
+	}
 }
 
-void InGameServer::PrepareToStartGame(int roomID, const std::array<int, MAX_ROOM_CAPACITY>& ids)
+void InGameServer::PrepareToStartGame(int roomID)
 {
-	// TODO: Initialize players..
+	for (const auto& player : msWorlds[roomID]->PlayerList)
+	{
+		const int hostID = player->ID;
+		if (hostID < 0) continue;
+
+		if (gClients[hostID]->ChangeState(CLIENT_STAT::IN_ROOM, CLIENT_STAT::IN_GAME) == false)
+		{
+			mLoginPtr->Disconnect(hostID);
+			continue;
+		}
+	}
+	SendGameStartSuccess(roomID);
 }
 
 bool InGameServer::ProcessPacket(std::byte* packet, char type, int id, int bytes)
@@ -26,9 +57,6 @@ bool InGameServer::ProcessPacket(std::byte* packet, char type, int id, int bytes
 	{
 	case CS::KEY_INPUT:
 	{
-#ifdef DEBUG_PACKET_TRANSFER
-		std::cout << "[" << id << "] Received key input packet.\n";
-#endif
 		CS::packet_key_input* pck = reinterpret_cast<CS::packet_key_input*>(packet);
 		if (pck->room_id < 0)
 		{
@@ -75,3 +103,35 @@ void InGameServer::HandleKeyInput(int id, uint8_t key, bool pressed)
 		break;
 	}
 }
+
+void InGameServer::SendGameStartSuccess(int roomID)
+{
+#ifdef DEBUG_PACKET_TRANSFER
+	std::cout << "[room id: " << roomID << "] Send game start packet.\n";
+#endif
+	SC::packet_game_start_success info_pck{};
+	info_pck.size = sizeof(SC::packet_game_start_success);
+	info_pck.type = SC::GAME_START_SUCCESS;
+	info_pck.room_id = roomID;
+	for (int i = 0; i < MAX_ROOM_CAPACITY; i++)
+	{
+		const btVector3 pos = msWorlds[roomID]->PlayerList[i]->GetPosition();
+		info_pck.x[i] = pos.x();
+		info_pck.y[i] = pos.y();
+		info_pck.z[i] = pos.z();
+	}
+	SendToAllPlayer(reinterpret_cast<std::byte*>(&info_pck), info_pck.size, roomID);
+}
+
+void InGameServer::SendToAllPlayer(std::byte* pck, int size, int roomID, int ignore, bool instSend)
+{
+	for (const auto& player : msWorlds[roomID]->PlayerList)
+	{
+		if (player->Empty == false && player->ID != ignore)
+		{
+			gClients[player->ID]->PushPacket(pck, size);
+			if (instSend) gClients[player->ID]->SendMsg();
+		}
+	}
+}
+
--- Server/InGameServer.h
+++ Server/InGameServer.h
@@ -1,32 +1,43 @@
 #pragma once
 
 #include "BPHandler.h"
-#include "Player.h"
 
 class LoginServer;
+class WaitRoom;
+class Player;
 
 struct GameWorld
 {
 	BPHandler Physics;
-	std::array<Player, MAX_ROOM_CAPACITY> PlayerList;
+	uint8_t MapIdx;
+	std::array<Player*, MAX_ROOM_CAPACITY> PlayerList;
 };
 
 class InGameServer
 {
+	using RoomList = std::array<std::unique_ptr<WaitRoom>, MAX_ROOM_SIZE>;
 	using WorldList = std::array<std::unique_ptr<GameWorld>, MAX_ROOM_SIZE>;
 public:
 	InGameServer();
 	~InGameServer() = default;
 
-	void Init(LoginServer* loginPtr);
-	void PrepareToStartGame(int roomID, const std::array<int, MAX_ROOM_CAPACITY>& ids);
+	void Init(LoginServer* loginPtr, RoomList& roomList);
+	void PrepareToStartGame(int roomID);
 
 	bool ProcessPacket(std::byte* packet, char type, int id, int bytes);
 	void HandleKeyInput(int id, uint8_t key, bool pressed);
 
+public:
+	void SendGameStartSuccess(int roomID);
+
+private:
+	void SendToAllPlayer(std::byte* pck, int size, int roomID, int ignore=-1, bool instSend=true);
 
 private:
 	LoginServer* mLoginPtr;
-
 	static WorldList msWorlds;
+
+	std::atomic_int mPlayerCount;
+
+	const btVector3 mStartPosition = { 500.0f, 10.0f, 500.0f };
 };
