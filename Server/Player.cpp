2a098b13265b1e3348a27bea2484108d8dd69637
Tomson0119 <kjs980119@gmail.com>
2022-02-07 18:08:53 +0900

    refactor - 플레이어 클래스에 정보들을 저장하여 관리한다.

--- Server/Player.cpp
+++ Server/Player.cpp
@@ -1,2 +1,14 @@
 #include "common.h"
 #include "Player.h"
+
+Player::Player()
+	: mPosition{ 0.0f, 0.0f, 0.0f },
+	  Empty{ true }, Color{ -1 }, Ready{ false }, 
+	  ID{ -1 }, Name{ }
+{
+}
+
+void Player::SetPosition(float x, float y, float z)
+{
+	mPosition = { x, y, z };
+}
--- Server/Player.h
+++ Server/Player.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include "MeshData.h"
+
 class Player
 {
 public:
-	Player() : mPosition{ btVector3(0.0f, 0.0f, 0.0f) } { }
-	~Player() { }
+	Player();
+	~Player() = default;
+
+	void SetPosition(float x, float y, float z);
+	const btVector3& GetPosition() const { return mPosition; }
+
+public:
+	bool Empty;
+	char Color;
+	bool Ready;
+	int ID;
+	char Name[MAX_NAME_SIZE];
 
 private:
 	btVector3 mPosition;
