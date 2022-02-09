5c058a2099a51b3aa243f6979cc5dc5e8235e6b5
Tomson0119 <kjs980119@gmail.com>
2022-02-09 15:35:03 +0900

    misc - meshdata 헤더 삭제

--- Server/BPHandler.h
+++ Server/BPHandler.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "MeshData.h"
-
 class BPHandler
 {
 public:
@@ -13,13 +11,10 @@ public:
 	void LoadCollisionMeshes();
 
 private:
-	// 나중에 저장하지 않도록 테스트
 	std::unique_ptr<btDefaultCollisionConfiguration> mBtCollisionConfiguration;
 	std::unique_ptr<btCollisionDispatcher> mBtDispatcher;
 	std::unique_ptr<btBroadphaseInterface> mBtOverlappingPairCache;
 	std::unique_ptr<btSequentialImpulseConstraintSolver> mBtSolver;
 
 	std::unique_ptr<btDiscreteDynamicsWorld> mBtDynamicsWorld;
-
-	//std::vector<std::unique_ptr<MeshData>> mMeshes;
 };
--- Server/Player.h
+++ Server/Player.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "MeshData.h"
-
 class Player
 {
 public:
