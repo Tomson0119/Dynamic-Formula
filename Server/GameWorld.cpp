9442cd2a92715d6638a5b72fa166806cb70dfd43
Tomson0119 <kjs980119@gmail.com>
2022-02-12 15:33:34 +0900

    feat - 물리엔진 업데이트, 패킷 전송 함수 이동.
    게임 시작 패킷을 월드에서 하도록 한다.
    물리엔진을 돌리기 전, 물리요소가 변했는지를 확인하여 업데이트해주고, 물리엔진을 돌린다.
    플레이어가 제거되면 해당 플레이어의 플래그를 deletion으로 바꿔준다.

--- Server/GameWorld.cpp
+++ Server/GameWorld.cpp
@@ -2,8 +2,13 @@
 #include "GameWorld.h"
 #include "WaitRoom.h"
 #include "Player.h"
+#include "Client.h"
+#include "LoginServer.h"
+#include "RigidBody.h"
 
 GameWorld::GameWorld()
+	: mID{ -1 },
+	  mPhysicsOverlapped{ OP::PHYSICS }
 {
 	for (int i = 0; i < mPlayerList.size(); i++)
 		mPlayerList[i] = nullptr;
@@ -16,15 +21,17 @@ void GameWorld::InitPhysics(float gravity)
 
 void GameWorld::InitMapRigidBody(BtTerrainShape* terrainShape, const std::vector<std::unique_ptr<BtBoxShape>>& objShapes)
 {
-	mMapRigidBody.CreateTerrainRigidBody(mPhysics.GetDynamicsWorld(), terrainShape);
+	mMapRigidBody.CreateTerrainRigidBody(terrainShape);
 	// TODO: CreateStaticObjectRigidBodies;
 }
 
-void GameWorld::InitPlayerList(const btVector3 startPosition, btScalar offsetX, RoomList& roomList)
+void GameWorld::InitPlayerList(const btVector3 startPosition, btScalar offsetX, WaitRoom* room)
 {
-	for (int i = 0; auto & player : mPlayerList)
+	mID = room->GetID();
+
+	for (int i = 0; auto& player : mPlayerList)
 	{
-		player = roomList[i]->GetPlayerPtr(i);
+		player = room->GetPlayerPtr(i);
 		player->SetPosition(
 			startPosition.x() + offsetX * i,
 			startPosition.y(),
@@ -35,6 +42,58 @@ void GameWorld::InitPlayerList(const btVector3 startPosition, btScalar offsetX, 
 
 void GameWorld::CreatePlayerRigidBody(int idx, btScalar mass, BtCarShape* shape)
 {
-	if (idx < 0) return;
 	mPlayerList[idx]->CreateVehicleRigidBody(mass, mPhysics.GetDynamicsWorld(), shape);
+}
+
+void GameWorld::UpdatePhysicsWorld(float timeStep)
+{
+	for (Player* player : GetPlayerList())
+		player->UpdateRigidBody(mPhysics.GetDynamicsWorld());
+
+	mMapRigidBody.UpdateRigidBody(mPhysics.GetDynamicsWorld());
+
+	mPhysics.StepSimulation(timeStep);
+}
+
+void GameWorld::RemovePlayerRigidBody(int idx)
+{
+	mPlayerList[idx]->SetDeletionFlag();
+}
+
+void GameWorld::SendGameStartSuccess()
+{
+#ifdef DEBUG_PACKET_TRANSFER
+	std::cout << "[room id: " << mID << "] Send game start packet.\n";
+#endif
+	SC::packet_game_start_success info_pck{};
+	info_pck.size = sizeof(SC::packet_game_start_success);
+	info_pck.type = SC::GAME_START_SUCCESS;
+	info_pck.room_id = mID;
+
+	for (int i = 0; i < MAX_ROOM_CAPACITY; i++)
+	{
+		const btVector3 pos = mPlayerList[i]->GetPosition();
+		info_pck.x[i] = pos.x();
+		info_pck.y[i] = pos.y();
+		info_pck.z[i] = pos.z();
+	}
+	SendToAllPlayer(reinterpret_cast<std::byte*>(&info_pck), info_pck.size);
+}
+
+WSAOVERLAPPEDEX* GameWorld::GetOverlapped(float timeStep)
+{
+	mPhysicsOverlapped.Reset(OP::PHYSICS, reinterpret_cast<std::byte*>(&timeStep), sizeof(timeStep));
+	return &mPhysicsOverlapped;
+}
+
+void GameWorld::SendToAllPlayer(std::byte* pck, int size, int ignore, bool instSend)
+{
+	for (const auto& player : mPlayerList)
+	{
+		if (player->Empty == false && player->ID != ignore)
+		{
+			gClients[player->ID]->PushPacket(pck, size);
+			if (instSend) gClients[player->ID]->SendMsg();
+		}
+	}
 }
--- Server/GameWorld.h
+++ Server/GameWorld.h
@@ -8,7 +8,6 @@ class WaitRoom;
 
 class GameWorld
 {
-	using RoomList = std::array<std::unique_ptr<WaitRoom>, MAX_ROOM_SIZE>;
 	using PlayerList = std::array<Player*, MAX_ROOM_CAPACITY>;
 
 public:
@@ -17,16 +16,27 @@ public:
 	
 	void InitPhysics(float gravity);
 	void InitMapRigidBody(BtTerrainShape* terrainShape, const std::vector<std::unique_ptr<BtBoxShape>>& objShapes);
-	void InitPlayerList(const btVector3 startPosition, btScalar offsetX, RoomList& roomList);
+	void InitPlayerList(const btVector3 startPosition, btScalar offsetX, WaitRoom* room);
 
 	void CreatePlayerRigidBody(int idx, btScalar mass, BtCarShape* shape);
+	void UpdatePhysicsWorld(float timeStep);
+
+	void RemovePlayerRigidBody(int idx);
+
+	void SendGameStartSuccess();
 
 public:
 	const PlayerList& GetPlayerList() const { return mPlayerList; }
+	WSAOVERLAPPEDEX* GetOverlapped(float timeStep);
+
+private:
+	void SendToAllPlayer(std::byte* pck, int size, int ignore=-1, bool instSend=true);
 
 public:
+	int mID;
+	WSAOVERLAPPEDEX mPhysicsOverlapped;
+
 	BPHandler mPhysics;
 	MapRigidBody mMapRigidBody;
-
 	PlayerList mPlayerList;
 };
