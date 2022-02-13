6a0642cf7d1abb2d83cca9b5d179c42bcb4a91ed
Tomson0119 <kjs980119@gmail.com>
2022-02-13 14:39:13 +0900

    misc - 불필요한 코드 제거 및 헤더 추가

--- Server/common.h
+++ Server/common.h
@@ -12,6 +12,7 @@
 #include <IOCP.h>
 #include <MemoryPool.h>
 
+#include <csignal>
 #include <deque>
 #include <chrono>
 #include <concurrent_priority_queue.h>
@@ -19,34 +20,13 @@
 #include "btBulletDynamicsCommon.h"
 #include "BulletCollision/CollisionShapes/btHeightfieldTerrainShape.h"
 
+#ifndef _DEBUG
+#define _DEBUG
+#else
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #define USE_DATABASE
 #define DEBUG_PACKET_TRANSFER
-
-namespace BulletHelper
-{
-	const float PI = 3.1415926535f;
-	inline btRigidBody* CreateRigidBody(btScalar mass, const btTransform& startTransform, btCollisionShape* shape, std::shared_ptr<btDiscreteDynamicsWorld> pbtDynamicsWorld)
-	{
-		btAssert((!shape || shape->getShapeType() != INVALID_SHAPE_PROXYTYPE));
-
-		bool isDynamic = (mass != 0.f);
-
-		btVector3 localInertia(0, 0, 0);
-		if (isDynamic && shape)
-			shape->calculateLocalInertia(mass, localInertia);
-
-		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
-
-		btRigidBody::btRigidBodyConstructionInfo cInfo(mass, myMotionState, shape, localInertia);
-
-		btRigidBody* body = new btRigidBody(cInfo);
-
-		pbtDynamicsWorld->addRigidBody(body);
-		return body;
-	}
-
-	inline float RadianToEuler(float fRadian)
-	{
-		return fRadian / 360 * (float)(2 * PI);
-	}
-}
