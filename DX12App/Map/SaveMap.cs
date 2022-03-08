3a3bdb9d3f6d7490f4fd34b6aa8fe61f4f751228
kcjsend2 <kcjsend2@naver.com>
2022-03-08 13:13:14 +0900

    맵 에디팅을 위한 유니티용 스크립트 추가, 오브젝트에 적용되는 텍스쳐를 기록하도록 변경

---
+++ DX12App/Map/SaveMap.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveMap : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start Writing\r\n");
+        FileStream file = new FileStream(Application.dataPath + "/MapData.tmap", FileMode.Create);
+        StreamWriter writer = new StreamWriter(file);
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            GameObject obj = transform.GetChild(i).gameObject;
+
+            //메쉬 이름
+            string name = obj.GetComponentInChildren<MeshFilter>().name;
+
+            //좌표
+            Vector3 pos = obj.transform.position;
+
+            //쿼터니언 회전각
+            Quaternion orientation = obj.transform.rotation;
+
+            string texture = obj.GetComponentInChildren<MeshFilter>().GetComponent<MeshRenderer>().materials[0].name;
+            texture = texture.Replace(" (Instance)", "");
+            
+
+            writer.Write(name + ".obj " + pos.x.ToString() + " " + pos.y.ToString() + " " + pos.z.ToString() +
+                " " + orientation.x.ToString() + " " + orientation.y.ToString() + " " + orientation.z.ToString() + " " + orientation.w.ToString() + " " + texture + "\r\n");
+            Debug.Log("Successfully Write " + name + "\r\n");
+        }
+        writer.Flush();
+        writer.Close();
+    }
+
+    void Update()
+    {
+        
+    }
+}
