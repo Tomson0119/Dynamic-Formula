ad6486fc5e651875b49418a89511012dccf049cd
Tomson0119 <kjs980119@gmail.com>
2022-03-17 09:47:15 +0900

    refactor - Endpoint 복사생성자 추가.

--- Server/Network/EndPoint.h
+++ Server/Network/EndPoint.h
@@ -5,9 +5,10 @@ class EndPoint
 public:
 	EndPoint() = default;
 	EndPoint(const std::string& ip, short port);
+	EndPoint(const sockaddr_in& addr);
 	~EndPoint();
 
 	static EndPoint Any(short port);
 
-	sockaddr_in address;
+	sockaddr_in mAddress;
 };
