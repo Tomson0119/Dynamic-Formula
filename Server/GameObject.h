df00b4270ea0261df46f76ff094becadaca5a563
Tomson0119 <kjs980119@gmail.com>
2022-04-14 11:43:44 +0900

    refactor - GameObject로 변경

---
+++ Server/GameObject.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class GameObject
+{
+protected:
+	enum class OBJ_TAG : int
+	{
+		NONE = 0,
+		VEHICLE,
+		MISSILE,
+		TRACK,
+		CHECKPOINT
+	};
+
+public:
+	virtual ~GameObject() = default;
+	virtual void UpdateRigidbodies(float elapsed, btDiscreteDynamicsWorld* physicsWorld) = 0;
+	virtual void Reset(btDiscreteDynamicsWorld* physicsWorld) = 0;
+	virtual void HandleCollisionWith(const btCollisionObject& objA, const btCollisionObject& objB, GameObject& otherObj) = 0;
+	virtual OBJ_TAG GetTag(const btCollisionObject& obj) const = 0;
+};
