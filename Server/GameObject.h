d53e417f770feb6b869bc79594518d9258924c60
Tomson0119 <kjs980119@gmail.com>
2022-04-24 20:37:25 +0900

    refactor - 오브젝트 태그는 GameObject에서 관리하지 않음.

--- Server/GameObject.h
+++ Server/GameObject.h
@@ -5,18 +5,8 @@ class BPHandler;
 class GameObject
 {
 public:
-	enum class OBJ_TAG : int
-	{
-		NONE = 0,
-		VEHICLE,
-		MISSILE,
-		TRACK,
-		CHECKPOINT
-	};
-
-public:
 	virtual ~GameObject() = default;
 	virtual void Update(float elapsed, BPHandler& physics) = 0;
 	virtual void Reset(BPHandler& physics) = 0;
-	virtual OBJ_TAG GetTag(const btCollisionObject& obj) const = 0;
+	virtual int GetMask(const btCollisionObject& obj) const = 0;
 };
