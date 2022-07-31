7739e1bbb0bb22cfdc3887edd45d28a92ac46fe3
Tomson0119 <kjs980119@gmail.com>
2022-07-31 15:07:52 +0900

    refactor - Interpolater 수정

--- DX12App/interpolator.h
+++ DX12App/interpolator.h
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <queue>
 
 class Interpolator
 {
@@ -6,19 +7,20 @@ public:
 	struct Entry
 	{
 		bool empty;
+		Clock::time_point timeStamp;
+
 		XMFLOAT3 position;
 		XMFLOAT4 rotation;
 
 		Entry()
-			: empty{ true },
-			position{},
-			rotation{}
+			: empty{ true }, position{}, rotation{}
 		{
 		}
 
 		Entry(const XMFLOAT3& pos, const XMFLOAT4& rot)
 			: empty{ false }
 		{
+			timeStamp = Clock::now();
 			position = pos;
 			rotation = rot;
 		}
@@ -26,114 +28,210 @@ public:
 
 public:
 	Interpolator()
-		: mCurrentTime{ 0 }, mPrevTimePoint{ 0 }
+		: mProgress{ 0.0f }
 	{
 	}
 
 	void Enqueue(uint64_t timePoint, const XMFLOAT3& pos, const XMFLOAT4& rot)
 	{
-		mEntryMapMut.lock();
-		mEntries.insert({ timePoint, Entry(pos, rot) });
-		mEntryMapMut.unlock();
+		mEntryQueueMut.lock();
+		mEntryQueue.push(Entry(pos, rot));
+		mEntryQueueMut.unlock();
 	}
 
-	void Interpolate(float dt, int64_t clockDelta, XMFLOAT3& targetPos, XMFLOAT4& targetRot)
+	void Interpolate(float dt, uint64_t timePoint, XMFLOAT3& targetPos, XMFLOAT4& targetRot)
 	{
-		mEntryMapMut.lock();
-		if (mEntries.empty())
+		mEntryQueueMut.lock();
+		if (mEntryQueue.empty())
 		{
-			mEntryMapMut.unlock();
+			mEntryQueueMut.unlock();
 			return;
 		}
-		mEntryMapMut.unlock();
 
+		Entry next = mEntryQueue.front();
 		if (mPrevEntry.empty)
 		{
-			FindNextEntry();
-			if (mPrevEntry.empty)
-			{
-				mEntryMapMut.lock();
-				mPrevTimePoint = mEntries.begin()->first;
-				mPrevEntry = mEntries.begin()->second;
-				mEntryMapMut.unlock();
-			}
-			targetPos = mPrevEntry.position;
-			targetRot = mPrevEntry.rotation;
+			mEntryQueue.pop();
+			mEntryQueueMut.unlock();
+
+			mPrevEntry = next;
+			targetPos = next.position;
+			targetRot = next.rotation;
 			return;
 		}
 
-		auto now = Clock::now().time_since_epoch().count();
-		auto now_ms = ConvertNsToMs(now) + clockDelta;
+		mEntryQueueMut.unlock();
 
-		//mCurrentTime = (uint64_t)(((float)mCurrentTime * PCT) + (float)now_ms * (1.0f - PCT));
-		mCurrentTime = now_ms;
+		mProgress += dt;
+		float timeBetween = GetDurationSec(next.timeStamp, mPrevEntry.timeStamp);
+		float progress = std::min(1.0f, mProgress / timeBetween);
 
-		auto next = FindNextEntry();
-		if (next.has_value() == false) {
-			return;
+		targetPos = Vector3::Lerp(mPrevEntry.position, next.position, progress);
+		targetRot = Vector4::Slerp(mPrevEntry.rotation, next.rotation, progress);
+
+		if (progress >= 1.0f)
+		{
+			mEntryQueueMut.lock();
+			mEntryQueue.pop();
+			mEntryQueueMut.unlock();
+
+			mPrevEntry = next;
+			mProgress -= timeBetween;
 		}
-
-		uint64_t nextTp = next.value().first;
-		Entry nextEntry = next.value().second;
-
-		if (nextTp <= mPrevTimePoint) return;
-
-		uint64_t timeBetween = nextTp - mPrevTimePoint;
-		float progress = (float)(mCurrentTime - mPrevTimePoint) / timeBetween;
-
-		Log::Print("prev time: ", mPrevTimePoint);
-		Log::Print("current time: ", mCurrentTime);
-		Log::Print("next time: ", nextTp);
-		Log::Print("progress: ", progress);
-
-		targetPos = Vector3::Lerp(mPrevEntry.position, nextEntry.position, progress);
-		targetRot = Vector4::Slerp(mPrevEntry.rotation, nextEntry.rotation, progress);
-
-		/*Print("prev position: ", mPrevEntry.position);
-		Print("current position: ", targetPos);
-		Print("next position: ", nextEntry.position);*/
-		//Log::Print("-----------------------------------", 0);
 	}
 
 	void Clear()
 	{
-		mEntryMapMut.lock();
-		mEntries.clear();
-		mEntryMapMut.unlock();
-
-		mCurrentTime = 0;
+		mEntryQueueMut.lock();
+		std::queue<Entry> temp{};
+		std::swap(mEntryQueue, temp);
+		mEntryQueueMut.unlock();
+		
+		mProgress = 0.0f;
 		mPrevEntry = Entry{};
 	}
 
 private:
-	std::optional<std::pair<uint64_t, Entry>> FindNextEntry()
+	static float GetDurationSec(Clock::time_point& a, Clock::time_point& b)
 	{
-		std::unique_lock lock{ mEntryMapMut };
-		while (mEntries.empty() == false &&
-			(mCurrentTime > mEntries.begin()->first))
-		{
-			mPrevTimePoint = mEntries.begin()->first;
-			mPrevEntry = mEntries.begin()->second;
-			mEntries.erase(mEntries.begin());
-		}
-
-		//Log::Print("Entries size: ", mEntries.size());
-
-		if (mEntries.empty())
-			return std::nullopt;
-
-		auto ret = *mEntries.begin();
-		//mEntries.erase(mEntries.begin());
-		return ret;
+		auto msec = std::chrono::duration_cast<std::chrono::milliseconds>
+			(a - b).count();
+		return (float)msec / 1000.0f;
 	}
 
 private:
-	std::map<uint64_t, Entry> mEntries;
-	std::mutex mEntryMapMut;
+	std::queue<Entry> mEntryQueue;
+	std::mutex mEntryQueueMut;
 
-	uint64_t mCurrentTime;
-	uint64_t mPrevTimePoint;
 	Entry mPrevEntry;
-
-	const float PCT = 0.05f;
+	float mProgress;
 };
+
+//#include "stdafx.h"
+//
+//class Interpolator
+//{
+//public:
+//	struct Entry
+//	{
+//		bool empty;
+//		XMFLOAT3 position;
+//		XMFLOAT4 rotation;
+//
+//		Entry()
+//			: empty{ true },
+//			position{},
+//			rotation{}
+//		{
+//		}
+//
+//		Entry(const XMFLOAT3& pos, const XMFLOAT4& rot)
+//			: empty{ false }
+//		{
+//			position = pos;
+//			rotation = rot;
+//		}
+//	};
+//
+//public:
+//	Interpolator()
+//		: mCurrentTime{ 0.0f }, mPrevTimePoint{ 0 }
+//	{
+//	}
+//
+//	void Enqueue(uint64_t timePoint, const XMFLOAT3& pos, const XMFLOAT4& rot)
+//	{
+//		mEntryMapMut.lock();
+//		mEntries.insert({ timePoint, Entry(pos, rot) });
+//		mEntryMapMut.unlock();
+//	}
+//
+//	void Interpolate(float dt, int64_t clockDelta, XMFLOAT3& targetPos, XMFLOAT4& targetRot)
+//	{
+//		mEntryMapMut.lock();
+//		if (mEntries.empty())
+//		{
+//			mEntryMapMut.unlock();
+//			return;
+//		}
+//		mEntryMapMut.unlock();
+//
+//		if (mPrevEntry.empty)
+//		{
+//			mEntryMapMut.lock();
+//			mPrevTimePoint = mEntries.begin()->first;
+//			mPrevEntry = mEntries.begin()->second;
+//			mEntryMapMut.unlock();
+//
+//			targetPos = mPrevEntry.position;
+//			targetRot = mPrevEntry.rotation;
+//			
+//			mCurrentTime = 0.0f;
+//			return;
+//		}
+//
+//		auto now = Clock::now().time_since_epoch().count();
+//		auto now_ms = ConvertNsToMs(now) + clockDelta;
+//
+//		auto next = FindNextEntry();
+//		if (next.has_value()) {
+//			mNextTimePoint = next.value().first;
+//			mNextEntry = next.value().second;
+//		}
+//
+//		uint64_t timeBetween = mNextTimePoint - mPrevTimePoint;
+//
+//		//mCurrentTime += dt;
+//		float progress = ((float)now_ms) / timeBetween;
+//
+//		Log::Print("timeBetween: ", timeBetween);
+//		Log::Print("now_ms: ", now_ms);
+//		Log::Print("progress: ", progress);
+//		Log::Print("---------------------------------");
+//
+//		targetPos = Vector3::Lerp(mPrevEntry.position, mNextEntry.position, progress);
+//		targetRot = Vector4::Slerp(mPrevEntry.rotation, mNextEntry.rotation, progress);
+//	}
+//
+//	void Clear()
+//	{
+//		mEntryMapMut.lock();
+//		mEntries.clear();
+//		mEntryMapMut.unlock();
+//
+//		mCurrentTime = 0;
+//		mPrevEntry = Entry{};
+//	}
+//
+//private:
+//	std::optional<std::pair<uint64_t, Entry>> FindNextEntry()
+//	{
+//		std::unique_lock lock{ mEntryMapMut };
+//		/*while (mEntries.empty() == false &&
+//			(mCurrentTime > mEntries.begin()->first))
+//		{
+//			mPrevTimePoint = mEntries.begin()->first;
+//			mPrevEntry = mEntries.begin()->second;
+//			mEntries.erase(mEntries.begin());
+//		}*/
+//
+//		if (mEntries.empty())
+//			return std::nullopt;
+//
+//		auto ret = *mEntries.begin();
+//		mEntries.erase(mEntries.begin());
+//		return ret;
+//	}
+//
+//private:
+//	std::map<uint64_t, Entry> mEntries;
+//	std::mutex mEntryMapMut;
+//
+//	uint64_t mPrevTimePoint;
+//	uint64_t mNextTimePoint;
+//	Entry mPrevEntry;
+//	Entry mNextEntry;
+//
+//	float mCurrentTime;
+//	const float PCT = 0.05f;
+//};
